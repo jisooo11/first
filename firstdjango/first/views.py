652c56b7baf4bf1dcacdc16cd0bc6ac707f939c2
jisooo11 <jisoolee11@kead.or.kr>
2025-03-05 14:58:28 +0900

    자기소개 만들기

--- firstdjango/first/views.py
+++ firstdjango/first/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # 화면에 글씨를 출력하는 코드
 def home(request):
-    context = {"name": "이지수", "food": "샤브샤브", "name3": "아무개"}
+    context = {"name": "이지수", "interest": "샤브샤브", "hobby": "아무개"}
     return render(request, "first/home.html", context)
